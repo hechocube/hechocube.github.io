71aac07e79b727a36b8b3be76c2066a11b2397a9
John Lloyd Mayol <jlmayol@dnamicro.com>
2022-03-19 10:49:00 +0800

    update: error importing js

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,24 @@ import "./assets/vendor/remixicon/remixicon.css";
 import "./assets/vendor/swiper/swiper-bundle.min.css";
 // Template Main CSS Files
 import "./assets/css/style.css";
+// Vendo JS Files
+import "./assets/vendor/purecounter/purecounter.js";
+import "./assets/vendor/aos/aos.js";
+import "./assets/vendor/bootstrap/js/bootstrap.bundle.min.js";
+import "./assets/vendor/glightbox/js/glightbox.min.js";
+import "./assets/vendor/isotope-layout/isotope.pkgd.min.js";
+import "./assets/vendor/swiper/swiper-bundle.min.js";
+import "./assets/vendor/php-email-form/validate.js";
+// Template Main JS File
+import "./assets/js/main.js";
 // Components
 import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
 
 ReactDOM.render(
   <React.StrictMode>
     <Navbar />
+    <Hero />
   </React.StrictMode>,
   document.getElementById("root")
 );
